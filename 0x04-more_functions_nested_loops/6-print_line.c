3b20d361ab4db9fcf9ea29427b156c9727f32c6d
Vikiey <ohaivicky00@gmail.com>
2022-09-15 19:59:17 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,16 @@ void print_line(int n)
 {
 	int a = 0;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		while (a <= n)
-		{
-			_putchar ('_');
-			a++;
-		}
 		_putchar ('\n');
 	}
 	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar ('_');
+		}
 		_putchar ('\n');
+	}
 }
