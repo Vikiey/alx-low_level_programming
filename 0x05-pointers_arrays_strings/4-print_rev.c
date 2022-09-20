bdd604f310d26ff459fbe747fe7652ce69ec01aa
Vikiey <ohaivicky00@gmail.com>
2022-09-19 18:08:36 -0700

    reversed string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev a function that prints a string, in reverse
+ * print_rev - a function that prints a string, in reverse
  * @s: pointer to the string
  * Return: string in reverse
  */
