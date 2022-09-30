7c73f39a77cdf25f8ea1f4729ef84897bec65253
Vikiey <ohaivicky00@gmail.com>
2022-09-30 17:43:59 +0100

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,8 +16,11 @@ int last_index(char *s)
 }
 
 /**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
+ * check - checker for the palindrome
+ * @s: string
+ * @start: int moves from right to left
+ * @end: int moves from left to right
+ * @pair: int
  * Return: 0 or 1
  */
 
@@ -31,18 +34,15 @@ int check(char *s, int start, int end, int pair)
 		return (check(s, start + 1, end - 1, pair));
 }
 
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
 int is_palindrome(char *s)
 {
 	int end = last_index(s);
 
 	return (check(s, 0, end - 1, end % 2));
 }
-
-/**
- * check - checker for the palindrome
- * @s: string
- * @start: int moves from right to left
- * @end: int moves from left to right
- * @pair: int
- * Return: 0 or 1
- */
