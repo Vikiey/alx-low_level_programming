5b0019f610865e40ee99a7e540cb43d1f4338815
Vikiey <ohaivicky00@gmail.com>
2022-10-28 00:04:32 +0100

    bits manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c1-print_binary.c
@@ -0,0 +1,19 @@
+/**
+ * get_bit - a function that gets a bit at index
+ * @n: the number to get bit from
+ * @index: the index where the bit get at
+ * Return: the value of the bit or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int max = 0x01;
+
+max <<= index;
+if (max == 0)
+return (-1);
+
+if ((n & max))
+return (1);
+else
+return (0);
+}
