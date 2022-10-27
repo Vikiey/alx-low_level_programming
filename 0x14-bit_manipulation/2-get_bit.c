afa4742b3b180450b0d4b6062a3c0eb3c8d23cac
Vikiey <ohaivicky00@gmail.com>
2022-10-27 16:29:40 -0700

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,14 +6,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int max = 0x01;
+	unsigned long int max = 0x01;
 
-max <<= index;
-if (max == 0)
-return (-1);
-
-if ((n & max))
-return (1);
-else
-return (0);
+	max <<= index;
+	if (max == 0)
+		return (-1);
+	if ((n & max))
+		return (1);
+	else
+		return (0);
 }
