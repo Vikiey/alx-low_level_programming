373caf761d925c5c109a63af9151d542cfd2794e
Vikiey <ohaivicky00@gmail.com>
2022-11-03 22:29:52 -0700

    malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,23 +8,22 @@
  * Return: pointer to a 2D array, NULL on failure
  */
 int **alloc_grid(int width, int height)
-{int **array, i = 0, j = 0;
-
-if (width <= 0 || height <= 0)
-return (NULL);
-
-array = (int **)malloc(sizeof(int *) * height);
-if (array == NULL)
-return (NULL);
-for (; i < height; i++)
 {
-array[i] = (int *)malloc(sizeof(int) * width);
-if (array[i] == NULL)
-return (NULL);
-}
+	int **array, i = 0, j = 0;
 
-for (i = 0; i < height; i++)
-for (; j < width; j++)
-array[i][j] = 0;
-return (array);
+	if (width <= 0 || height <= 0)
+		return (NULL);	
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < height; i++)
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
 }
