f8348f64babd838ffe92ba177c5ba023fd2deacf
Vikiey <ohaivicky00@gmail.com>
2022-11-03 22:52:20 -0700

    malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dim. array.
+* ch_free_grid - frees a 2 dim. array.
 * @grid: multidimensional array of char.
 * @height: height of the array.
 * Return: no return
@@ -10,13 +10,13 @@
 
 void ch_free_grid(char **grid, unsigned int height)
 {
-if (grid != NULL && height != 0)
-{
-for (; height > 0; height--)
-free(grid[height]);
-free(grid[height]);
-free(grid);
-}
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+			free(grid[height]);
+			free(grid);
+	}
 }
 
 /**
@@ -27,40 +27,41 @@ free(grid);
 
 char **strtow(char *str)
 {
-char **aout;
-unsigned int c, height, i, j, a1;
-if (str == NULL || *str == '\0')
-return (NULL);
-for (c = height = 0; str[c] != '\0'; c++)
-if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-height++;
-aout = malloc((height + 1) * sizeof(char *));
-if (aout == NULL || height == 0)
-{
-free(aout);
-return (NULL);
-}
-for (i = a1 = 0; i < height; i++)
-{
-for (c = a1; str[c] != '\0'; c++)
-{
-if (str[c] == ' ')
-a1++;
-if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-{
-aout[i] = malloc((c - a1 + 2) * sizeof(char));
-if (aout[i] == NULL)
-{
-ch_free_grid(aout, i);
-return (NULL);
-}
-break;
-}
-}
-for (j = 0; a1 <= c; a1++, j++)
-aout[i][j] = str[a1];
-aout[i][j] = '\0';
-}
-aout[i] = NULL;
-return (aout);
+	char **aout;
+	unsigned int c, height, i, j, a1;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			height++;
+	aout = malloc((height + 1) * sizeof(char *));
+	if (aout == NULL || height == 0)
+	{
+		free(aout);
+		return (NULL);
+	}
+	for (i = a1 = 0; i < height; i++)
+	{
+		for (c = a1; str[c] != '\0'; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					ch_free_grid(aout, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+			aout[i][j] = '\0';
+	}
+	aout[i] = NULL;
+	return (aout);
 }
