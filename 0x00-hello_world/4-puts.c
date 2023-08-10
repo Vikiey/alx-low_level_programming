7e3147bf7f8381e2d89777d937a2356edadf8cb3
Vikiey <ohaivicky00@gmail.com>
2023-08-10 06:15:25 -0700

    ueing puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - a function
+ * return: 0 if success and exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
