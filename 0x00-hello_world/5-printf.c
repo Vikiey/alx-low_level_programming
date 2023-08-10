a70412dc25ba0f118e19cffc6b01d020f57761c6
Vikiey <ohaivicky00@gmail.com>
2023-08-10 06:36:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using printf followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
