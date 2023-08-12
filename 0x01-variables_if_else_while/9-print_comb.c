771d38bb2e1319e4372c6d11a2da3aa00fd19dce
Vikiey <ohaivicky00@gmail.com>
2023-08-12 01:45:53 -0700

    double digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 100; i++)
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
