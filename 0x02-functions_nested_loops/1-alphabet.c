6002413227ff80032d9455d8559c37e0664d497b
Vikiey <ohaivicky00@gmail.com>
2023-08-15 13:47:58 -0700

    ...

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints the alphabet in lowercase then a new line
+ */
+
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 97; lower <= 122; lower++)
+		_putchar(lower);
+	_putchar('\n');
+}
