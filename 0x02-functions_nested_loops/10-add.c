8b348bbbc0f941056b04d316e6b81927462c606a
Vikiey <ohaivicky00@gmail.com>
2023-08-16 16:11:47 -0700

    ..

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,14 @@
 
 /**
  * add - a function that adds two integers and returns the result
+ * @a: integer value
+ * @b: integer value
+ * Return: sum of two integers
  */
 
 int add(int a, int b)
 {
-	sum = a + b;
+	int sum = a + b;
 
 	return (sum);
 }
