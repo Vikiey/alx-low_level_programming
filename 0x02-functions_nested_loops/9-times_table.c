d21c08ec2dcf16f5cde784c25711722bfc310b0a
Vikiey <ohaivicky00@gmail.com>
2023-08-16 16:23:33 -0700

    ..

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,16 @@ void times_table(void)
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(0);
-		
+
 		for (j = 0; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			k = i * j;
 
-			if (j < 9)
+			if (k < 9)
 			{
-				_putchar(',');
 				_putchar(' ');
 			}
 			else
