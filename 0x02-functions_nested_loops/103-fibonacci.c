dc38b7384c3aa53eba6d84c063bc9ddb75b020b5
Vikiey <ohaivicky00@gmail.com>
2023-08-17 01:09:42 +0100

    tired i am

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints fibonacci
+ * Return: 0
+*/
+
+int main(void)
+{
+    unsigned long a = 0, n = 1, b = 0, sum = 0;
+while (b <= 4000000)
+{
+b = a + n;
+a = n;
+n = b;
+
+if ((a % 2) == 0)
+sum += a;
+
+}
+printf("%ld\n", sum);
+return (0);
+}
