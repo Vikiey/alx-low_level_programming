ff9aa71539ce54addda5f97fc01ace9b77123edc
Vikiey <ohaivicky00@gmail.com>
2023-08-21 20:59:35 -0700

    ..

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", a[i]);
+		printf("%d", a[i]);
 
-		if (i != j)
+		if (i < j)
 			printf(", ");
 	}
-	_putchar('\n');
+	printf("\n");
 }
