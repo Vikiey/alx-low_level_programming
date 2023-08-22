9c5d1f450ad60e07fbf0f6442a8d13c5bd7208f7
Vikiey <ohaivicky00@gmail.com>
2023-08-21 21:45:34 -0700

    ..

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to char string
+ * Return: 0 (success)
+ */
+
+int _atoi(char *s)
+{
+	int a = 0;
+	int b = 0;
+	int sign = 1;
+
+	while (s[a] == ' ')
+		a++;
+	if (s[a] == '-')
+	{
+		sign *= -1;
+		a++;
+	}
+	while (s[a] >= '0' && s[a] <= 9)
+	{
+		b = b * 10 + (s[a] - '0');
+		a++;
+	}
+
+	return (b * a);
+}
