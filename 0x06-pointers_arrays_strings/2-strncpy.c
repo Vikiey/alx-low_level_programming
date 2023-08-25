49c089455caa4370563021435656f4e815ad2fd1
Vikiey <ohaivicky00@gmail.com>
2023-08-24 17:19:02 -0700

    C...

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - a function that copies a string
+ * @dest: char string
+ * @src: char string
+ * @n: number of bytes to copy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int p;
+
+	/* copies up to "n" characters or '\0' */
+	for (p = 0; p < n && src[p] != '\0'; p++)
+		dest[p] = src[p];
+
+	/* if n > length of src add null byte to fipl dest string */
+	if (p < n)
+	{
+		dest[p] = '\0';
+		p++;
+	}
+	return (dest);
+}
