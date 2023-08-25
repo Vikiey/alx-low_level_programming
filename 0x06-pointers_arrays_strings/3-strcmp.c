786de72e9afc986e1a4f02dbf6c118f63ca519f5
Vikiey <ohaivicky00@gmail.com>
2023-08-24 18:57:11 -0700

    C...

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,6 +14,8 @@ int _strcmp(char *s1, char *s2)
 	for (n = 0; s1[n] != '\0' && s2[n] != '\0'; n++)
 	{
 		if (s1[n] != s2[n])
-			return (s1[n] - s2[n]);
+			break;
 	}
+
+	return (s1[n] - s2[n]);
 }
