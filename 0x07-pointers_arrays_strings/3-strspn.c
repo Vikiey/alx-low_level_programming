c16f3c09569cdcbe2d9437cdcd5d0a626b00eddb
Vikiey <ohaivicky00@gmail.com>
2023-08-29 17:34:19 -0700

    C...

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int num;
 	int a = 0;
-	int b = 0;
-	int c;
+	int b, c;
 
 	while (s[a] != '\0')
 	{
+		b = 0;
 		for (c = 0; accept[c] != '\0'; c++)
 		{
 			if (s[a] == accept[c])
