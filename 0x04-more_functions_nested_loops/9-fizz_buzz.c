6a0e735e5f4da34f459cb69762b09427c69ace2c
Vikiey <ohaivicky00@gmail.com>
2023-09-11 05:19:39 -0700

    C...

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,9 @@
 
 /**
  * main - fizz buzz test
+ * Description: prints 1 to 100 then a new line
+ * prints Fizz for multiples of 3, Buzz for multiples of 5
+ * and prints Fizzbuzz for multiples of 3 and 5
  * Return: 0
  */
 
@@ -11,8 +14,8 @@ int main(void)
 
 	for (b = 1; b <= 100; b++)
 	{
-		if ((b % 3 == )0 && (b % 5 == )0)
-			printf("Fizzbuzz");
+		if (b % 3 == 0 && b % 5 == 0)
+			printf("FizzBuzz");
 		else if (b % 3 == 0)
 			printf("Fizz");
 		else if (b % 5 == 0)
