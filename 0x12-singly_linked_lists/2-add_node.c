6ad3003a4f8b21a200683eabfed33e1ac76be6ef
Vikiey <ohaivicky00@gmail.com>
2023-09-22 09:39:43 -0700

    new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,12 +9,17 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	int len = 0;
 	list_t *new;
+
+	while (str[len] != '\0')
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
+	new->len = len;
 	new->str = strdup(str); /* duplicate the string */
 	if (new->str == NULL)
 	{
