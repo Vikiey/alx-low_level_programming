ac69f23d35a589bd895107182b7ff6373f6a9cec
Vikiey <ohaivicky00@gmail.com>
2023-09-25 15:44:36 -0700

    ...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * wildcmp - a function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if s1 == s2 otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2 || *s2 == 0)
+		if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	return (0);
+}
