e0ee4bbb3f0508b6f450abf97e2be5853c1b82ae
Vikiey <ohaivicky00@gmail.com>
2023-09-26 16:10:57 -0700

    C...

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,37 +5,37 @@
 /**
  * *argstostr - concatenates all the arguments of your program
  * @ac: argument count
- * @av: argument vector
+ * @av: array of characters
  * Return: pointer to a new string, or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
 	char *cate;
-	int a, b, len;
-	int point = 0;
+	int a, b;
+	int point = 0, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (a = 0; a < ac; a++)
 	{
-		for (b = 0; av[a][b]; b++)
+		for (b = 0; av[a][b] != '\0'; b++)
 			len++; /*calculates the total length */
 	}
 
-	cate = malloc(len + 1);
+	cate = malloc(sizeof(char) * len + 1);
 
 	if (cate == NULL)
 		return (NULL);
 
 	for (a = 0; a < ac; a++)
 	{
-		for (b = 0; av[a][b]; b++)
+		for (b = 0; av[a][b] != '\0'; b++)
 			cate[point++] = av[a][b];
 		cate[point++] = '\n';
 	}
-	cate[len] = '\0';
+	cate[point] = '\0';
 
 	return (cate);
 }
