5d63f8d6eadf12b3bd03180fc246b54e2fbc7a2f
Vikiey <ohaivicky00@gmail.com>
2023-11-19 01:12:34 -0800

    :)

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* convert the arguments to integers */
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	prod = x * y;
+	printf("%d\n", prod);
+
+	return(0);
+}
