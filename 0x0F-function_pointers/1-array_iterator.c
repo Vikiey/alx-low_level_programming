3baf882464f80bc673385f36e99840e594f5606b
Vikiey <ohaivicky00@gmail.com>
2023-11-29 08:15:07 -0800

    ...

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function that executes a function
+ * given as a parameter on each element of an array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to the function you need to use
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: type int
+ * @b: type int
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - prints the difference between two numbers
+ * @a: type int
+ * @b: type int
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: type int
+ * @b: type int
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides a by b
+ * @a: type int
+ * @b: type int
+ * Return: division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the remainder of a divided by b
+ * @a: type int
+ * @b: type int
+ * Return: remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
