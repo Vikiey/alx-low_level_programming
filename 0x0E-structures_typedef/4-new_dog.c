ef0d22d3f0faaa48a3aaee7d902421e25222718e
Vikiey <ohaivicky00@gmail.com>
2023-11-28 16:25:47 -0800

    :)

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * *new_dog - a function that creates a new dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: NULL if it fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
