6f5ac407f652edb86837379af7fa7837f3fae7f3
Vikiey <ohaivicky00@gmail.com>
2023-12-07 16:01:37 -0800

    ...

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print - uses printf to print
+ * __attribute__((constructor)): attribute used to specify that
+ * 'print' function should be executed before the main function
+ */
+
+void __attribute__((constructor)) print(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+An intro into single linked lists
